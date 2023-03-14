a9217d811c8c9a9d3c7f660db144fadafa2c2b23
vms16 <vms16@Sumanth>
2023-03-14 11:36:17 +0530

    Haudu

--- toLearnGitHub/src/toLearnGitHub1/WorkSmarter.java
+++ toLearnGitHub/src/toLearnGitHub1/WorkSmarter.java
@@ -5,7 +5,6 @@ public class WorkSmarter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiii");
-
+		System.out.println("May the conflict be displayed");
 	}
-
 }
